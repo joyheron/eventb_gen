0bdab7fe1850cb96aa7ca50625e1fbffb4ab678b
joyclark <clark.joy@gmail.com>
2015-09-07 15:27:37 +0200

    updated main so that the generator can be run via command line.

--- src/main/java/de/prob2/gen/Main.java
+++ src/main/java/de/prob2/gen/Main.java
@@ -1,24 +1,77 @@
 package de.prob2.gen;
 
-public class Main {
-	private static Thread prob;
+import java.io.IOException;
 
-	public static void runProB(final de.prob.Main m, final String[] args) {
-		prob = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				m.main(args);
-			}
-		});
-		prob.start();
-	}
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+import de.be4.eventbalg.core.parser.BException;
+
+public class Main {
+	public final static String NAME = "name";
+	public final static String DEBUG = "debug";
+	public final static String PATH = "path";
 
 	/**
 	 * @param args
 	 */
 	public static void main(final String[] args) {
-		de.prob.Main m = de.prob.Main.getInjector().getInstance(
-				de.prob.Main.class);
-		runProB(m, args);
+		PosixParser parser = new PosixParser();
+		Options options = getCommandlineOptions();
+		try {
+			CommandLine line = parser.parse(options, args);
+			String name = "modelgen";
+			if (line.hasOption(NAME)) {
+				name = line.getOptionValue(NAME);
+			}
+			boolean debug = false;
+			if (line.hasOption(DEBUG)) {
+				debug = true;
+			}
+
+			new ModelGenerator(line.getOptionValue(PATH), name, debug);
+		} catch (ParseException exp) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("java -jar probcli.jar", options);
+			System.exit(-1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (BException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@SuppressWarnings("static-access")
+	public static Options getCommandlineOptions() {
+		Options options = new Options();
+
+		Option path = OptionBuilder
+				.withArgName("path/to/dir")
+				.hasArg()
+				.withDescription(
+						"specify the directory which contains the model description files (.emch for machines, .ctx for contexts)")
+						.create(PATH);
+
+		Option name = OptionBuilder.withArgName("name").hasArg()
+				.withDescription("specify the name for the generated project")
+				.create(NAME);
+
+		Option debug = new Option(DEBUG,
+				"print debug information during project generation");
+
+		OptionGroup required = new OptionGroup();
+		required.setRequired(true);
+		required.addOption(path);
+		options.addOptionGroup(required);
+		options.addOption(name);
+		options.addOption(debug);
+		return options;
 	}
 }
--- src/main/java/de/prob2/gen/ModelGenerator.java
+++ src/main/java/de/prob2/gen/ModelGenerator.java
@@ -1,35 +1,110 @@
 package de.prob2.gen;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
 import de.be4.eventbalg.core.parser.BException;
 import de.be4.eventbalg.core.parser.EventBParser;
-import de.be4.eventbalg.core.parser.analysis.ASTPrinter;
 import de.be4.eventbalg.core.parser.node.Start;
 import de.prob.Main;
 import de.prob.model.eventb.EventBModel;
+import de.prob.model.eventb.translate.ModelToXML;
 import de.prob.scripting.StateSpaceProvider;
 
 public class ModelGenerator {
 
 	private EventBModel model;
 
-	public ModelGenerator() {
-		model = new EventBModel(Main.getInjector().getInstance(
+	public ModelGenerator(String path, String projectName, boolean debug)
+			throws IOException, BException {
+		EventBModel model = new EventBModel(Main.getInjector().getInstance(
 				StateSpaceProvider.class));
+		File file = new File(path);
+		checkFile(file, true);
+		File[] files = file.listFiles(new FilenameFilter() {
+
+			@Override
+			public boolean accept(File dir, String name) {
+				String lowercaseName = name.toLowerCase();
+				if (lowercaseName.endsWith(".emch")
+						|| lowercaseName.endsWith(".ctx")) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		});
+		for (File f : files) {
+			checkFile(f, false);
+			if (debug) {
+				System.out.println("viewing file " + file.getAbsolutePath());
+			}
+			String text = readFile(f);
+			if (debug) {
+				System.out.println("extracting file " + file.getAbsolutePath());
+			}
+			model = addComponent(model, text);
+			if (debug) {
+				System.out.println("file extraction sucessful for "
+						+ file.getAbsolutePath());
+				System.out.println("Model is now: " + model.toString());
+			}
+
+		}
+		this.model = model;
+		if (debug) {
+			System.out.println("writing to Rodin");
+		}
+		new ModelToXML().writeToRodin(model, projectName, path);
+
+		System.out.println("Rodin project written to: " + path + projectName
+				+ File.separator);
+
+	}
+
+	public String readFile(File file) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		try {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			return sb.toString();
+		} finally {
+			br.close();
+		}
+	}
+
+	public void checkFile(File file, boolean directory) throws IOException {
+		if (!file.exists()) {
+			throw new FileNotFoundException(file.getAbsolutePath());
+		} else if (file.isDirectory() != directory) {
+			String expected = directory ? "directory" : "file";
+			String was = directory ? "file" : "directory";
+			throw new IOException("Expected " + file.getAbsolutePath()
+					+ " to be a " + expected + " but was a " + was);
+		}
 	}
 
 	public EventBModel getModel() {
 		return model;
 	}
 
-	public EventBModel addComponent(final String componentDesc)
-			throws BException {
+	public EventBModel addComponent(EventBModel model,
+			final String componentDesc) throws BException {
 		EventBParser parser = new EventBParser();
-		Start ast = parser.parse(componentDesc, true);
-		ast.apply(new ASTPrinter());
+		Start ast = parser.parse(componentDesc, false);
 		ModelExtractor modelE = new ModelExtractor(model);
 		ast.apply(modelE);
-		model = modelE.getModel();
-		return model;
+		return modelE.getModel();
 	}
 
 }
